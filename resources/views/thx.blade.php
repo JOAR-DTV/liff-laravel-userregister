a2a609f0f6087dbe0b399e2e4cb867f796cf5ed1
EC2 Default User <ec2-user@ip-172-31-47-214.ec2.internal>
2019-08-11 04:48:34 +0000

    finish the liff application

--- resources/views/thx.blade.php
+++ resources/views/thx.blade.php
@@ -2,6 +2,14 @@
 @section('content')
 
 <h1>ご登録ありがとうございました！</h1>
+<button type="button" class="btn btn-success" id="close">登録を終了する</button>
+<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+<script>
+    document.getElementById('close').addEventListener('click', function(){
+        liff.closeWindow();
+    });
+</script>
+
 
 
 @endsection
