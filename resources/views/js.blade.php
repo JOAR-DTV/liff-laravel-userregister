bf728d5f216ac050366b496ab40cdfc30fba6fd3
EC2 Default User <ec2-user@ip-172-31-47-214.ec2.internal>
2019-08-11 07:52:40 +0000

    js

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -3,7 +3,7 @@
 <head>
 <title>user register</title>
 <!-- CSS と JavaScript -->
-<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+<script src="https://d.line-scdn.net/liff/1.0/sdk.js" type="text/javascript"></script>
 <script src="{{ asset('/js/liff.js') }}"></script>
 </head>
 <body>
